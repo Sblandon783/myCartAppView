51821c0d92077bd19902a832b17baccbbbdfc26e
steven@sdc.com <Steven-at-489759090577>
2023-12-03 16:45:04 -0600

    added new view to mathc

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "01b639592dcb39554046132007661d94",
-"index.html": "c91e89f0bcc3fa7bb2a14db00a4ffcd2",
-"/": "c91e89f0bcc3fa7bb2a14db00a4ffcd2",
-"main.dart.js": "66753661c7aba4ea9b808b2313795378",
+"index.html": "4471b75271408bcdbc7b345b1295a670",
+"/": "4471b75271408bcdbc7b345b1295a670",
+"main.dart.js": "9aba7c7920527511393b36c4993e5a6c",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
