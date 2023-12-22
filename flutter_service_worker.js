1ea53758634c508596c6da7069d3b1c65572afe7
steven@sdc.com <Steven-at-489759090577>
2023-12-22 14:13:19 -0600

    Added perfomance and two teams

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "01b639592dcb39554046132007661d94",
-"index.html": "f64ab929ee4eccdf445a980fdba6bdba",
-"/": "f64ab929ee4eccdf445a980fdba6bdba",
-"main.dart.js": "ada89dbd5773d2dca279a823b3991c4d",
+"index.html": "f5be9006f18a53f7020ebeb3affd02e4",
+"/": "f5be9006f18a53f7020ebeb3affd02e4",
+"main.dart.js": "e9b528a05b5429e79e4395d4e996fbdf",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
