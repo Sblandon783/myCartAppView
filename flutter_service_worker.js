faf7fb0bdc07e240d7e4d58034e90d3b561a918e
steven@sdc.com <Steven-at-489759090577>
2024-01-03 16:47:06 -0600

    added

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "01b639592dcb39554046132007661d94",
-"index.html": "2115e8ca7f3838a163946003b2edeec8",
-"/": "2115e8ca7f3838a163946003b2edeec8",
-"main.dart.js": "86b5a66a3305ede23dfc1838ead8bfc2",
+"index.html": "afb537bc24653f922f8f854d8bbf2a5e",
+"/": "afb537bc24653f922f8f854d8bbf2a5e",
+"main.dart.js": "bdfa6fa1d8f9bd4a23dfe7c6d7fa9890",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
